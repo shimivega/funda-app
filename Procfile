7e62c31f232a2200d9cdf6252a0279a61e2cbbb9
amukelani <133163276+shimivega@users.noreply.github.com>
2025-07-16 11:36:05 +0000

    Deployable version

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT main:app
-release: python -c "from app import app, db; app.app_context().push(); db.create_all()"
+web: gunicorn app:app
--- main.py
+++ main.py
@@ -398,3 +398,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('DEBUG', 'False').lower() == 'true'
     app.run(host='0.0.0.0', port=port, debug=debug)
+return "Hello from Funda App!"
